c3352af28e4766c0af93366ff16d96b5403b6dc2
Leonid <leonid0991@gmail.com>
2023-05-25 16:36:09 +0300

    limitter

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose.connect(
 );
 
 app.use(requestLogger);
-// app.use(limiter);
+app.use(limiter);
 app.use(cors());
 app.options('*', cors()); // enable requests for all routes
 app.use(helmet());
