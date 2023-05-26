f34b6fa480d4ef52b34be8b5840492da48a2e365
Leonid <leonid0991@gmail.com>
2023-05-26 13:21:40 +0300

    helmet

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(requestLogger);
 app.use(cors());
 app.options('*', cors()); // enable requests for all routes
 
-
-// app.use(helmet());
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
